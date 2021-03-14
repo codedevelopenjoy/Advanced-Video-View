462e4b5596a5199ac40c1ba23c7882698beb78c8
codedevelopenjoy <hardikgoel19@gmail.com>
2021-03-14 22:27:37 +0530

    First Commit

---
+++ app/src/main/java/com/video/player/advanced/MainActivity.java
@@ -0,0 +1,13 @@
+package com.video.player.advanced;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
